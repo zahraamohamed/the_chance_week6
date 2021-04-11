7ae1a40c9a785f9d4f26c6acdc14b3397fa16d76
Bareq H. AL-Taamah <bareqaltaamah@gmail.com>
2021-04-11 21:44:55 +0300

    add business logic

---
+++ app/src/main/java/com/ibareq/the_chance_week6/LoginUtil.kt
@@ -0,0 +1,13 @@
+package com.ibareq.the_chance_week6
+
+object LoginUtil {
+
+    fun login(email: String, password: String){
+        TODO("Not yet implemented")
+    }
+
+    fun logout(){
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/ibareq/the_chance_week6/MainActivity.kt
+++ app/src/main/java/com/ibareq/the_chance_week6/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ibareq.the_chance_week6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickLogin(v: View){
+        LoginUtil.login("test@mail.com", 1234)
     }
 }
